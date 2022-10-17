81571cff3faa6eafdd9e38ed053cd587b8bc95e3
Pinheiro-Lucas <lucas.gpinheiro2002@gmail.com>
2022-10-17 15:31:33 -0300

    :art: Improved project structure + database stuff

--- app.py
+++ app.py
@@ -1,9 +1,51 @@
 """
-Why this file exists? It's easier to run the application
 - python -m flask run
 - python app.py
 """
 
-from src.main import app
+from flask import Flask, session, request, redirect, url_for, render_template, flash, current_app
+from dotenv import dotenv_values
+from src.database import Database
 
-app.run()
+# Return a dict with everything from .env
+config = dotenv_values()
+
+db = Database(config)
+app = Flask(__name__)
+
+
+# Routes
+@app.route('/', methods=['GET'])
+def index():
+    return render_template("base.html"), 200
+
+
+@app.route('/find/<table>', methods=['GET'])
+def find_all(table):
+    return db.find(table)
+
+
+@app.route('/find/<table>/<idt>', methods=['GET'])
+def find(table, idt):
+    ids = {
+        "post": "post_id",
+        "post_category": "category_id",
+        "post_image": "post_images_id"
+    }
+
+    return db.find(table, ids[table], idt)
+
+
+@app.route('/delete/<table>/<idt>', methods=['GET'])
+def find(table, idt):
+    ids = {
+        "post": "post_id",
+        "post_category": "category_id",
+        "post_image": "post_images_id"
+    }
+
+    return db.find(table, ids[table], idt)
+
+
+if __name__ == "__main__":
+    app.run()
--- src/database.py
+++ src/database.py
@@ -1,23 +1,63 @@
-from flask_sqlalchemy import SQLAlchemy
-from main import app, config
+# from flask_sqlalchemy import SQLAlchemy
+import mysql.connector as mysql
 
 """
-    Important Stuff:
-        - Using sqlalchemy to have sql flexibility (ORM)
+    Future Stuff:
+        - Use an ORM to have sql flexibility (sqlalchemy)
         - SGBD/DBMS: "Database management System", the project is using mysql-connector-python by default
         - Connection stuff is in .env file
-        - URI: Identifies a resource
 """
 
-app.config["SQLALCHEMY_DATABASE_URI"] = \
-    f"{config['DB_SYSTEM']}://" \
-    f"{config['DB_USER']}:" \
-    f"{config['DB_PASSWORD']}@" \
-    f"{config['DB_HOST']}:" \
-    f"{config['DB_PORT']}/" \
-    f"{config['DB_NAME']}"
+# app.config["SQLALCHEMY_DATABASE_URI"] = \
+#     f"{config['DB_SYSTEM']}://" \
+#     f"{config['DB_USER']}:" \
+#     f"{config['DB_PASSWORD']}@" \
+#     f"{config['DB_HOST']}:" \
+#     f"{config['DB_PORT']}/" \
+#     f"{config['DB_NAME']}"
+
+# db = SQLAlchemy(app)
 
 
-db = SQLAlchemy(app)
+class Database:
+    def __init__(self, config: dict):
+        try:
+            self.db = mysql.connect(
+                host=config['DB_HOST'],
+                port=config['DB_PORT'],
+                database=config['DB_NAME'],
+                user=config['DB_USER'],
+                password=config['DB_PASSWORD']
+            )
+        except mysql.Error as err:
+            # Print the problem and exit the application
+            print(f"{err.errno} | {err.msg}")
+            exit()
 
-# Todo: Tables
+    def execute(self, command: str):
+        cursor = self.db.cursor()
+        # Just to avoid syntax errors
+        command = command + ";" if command[-1] != ";" else command
+
+        # Execute and commit
+        cursor.execute(command)
+        self.db.commit()
+        cursor.close()
+
+    def find(self, table: str, column=False, id=False):
+        cursor = self.db.cursor()
+        if not column and not id:
+            sql = f"SELECT * FROM {table};"
+        else:
+            sql = f"SELECT * FROM {table} WHERE {column}={id};"
+
+        cursor.execute(sql)
+
+        return cursor.fetchall()
+
+    def delete(self, table, column, info):
+        cursor = self.db.cursor()
+        cursor.execute("DELETE FROM %s WHERE %s = %s", (table, column, info))
+
+        self.db.commit()
+        cursor.close()
